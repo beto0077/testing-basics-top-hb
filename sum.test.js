14f8226fbba7f00d99819fdb902c3cdc6d5a838e
beto0077 <131410367+beto0077@users.noreply.github.com>
2026-03-11 19:57:40 -0600

    Basic tests completed

---
+++ sum.test.js
@@ -0,0 +1,10 @@
+import { test, expect } from "@jest/globals";
+import { sum } from "./sum";
+
+test("adds 1 + 2 to equal 3", () => {
+  expect(sum(1, 2)).toBe(3);
+});
+
+test("adds 3 + 2 to equal 5", () => {
+  expect(sum(3, 2)).toBe(5);
+});
